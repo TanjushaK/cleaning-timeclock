bb2891cdbbec1abfbba6a38910883e0ac64e6933
TanjushaK <ziuzia1503@gmail.com>
2026-02-27 21:59:32 +0100

    chore: remove legacy duplicates folder

--- __legacy_root_duplicates__20260227-182105/_components/AppFooter.tsx
+++
@@ -1,8 +0,0 @@
-export default function AppFooter() {
-  const year = new Date().getFullYear()
-  return (
-    <footer className="appFooter">
-      Чисто. Чётко. По времени. <span className="appFooterYear">© {year}</span>
-    </footer>
-  )
-}
